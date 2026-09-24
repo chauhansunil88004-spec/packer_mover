df4a7c122aacda76c9773234a90442458f20d43c
chauhansunil88004-spec <ecademyhub@gmail.com>
2026-09-24 15:49:44 +0530

    feat: direct src at root level for new username layout

--- src/Compnents/Topbar.jsx
+++ src/Compnents/Topbar.jsx
@@ -8,7 +8,7 @@ import {
   FileText,
   AlertCircle
 } from "lucide-react";
-import "./TopBar.css";
+import "./Topbar.css";
 
 const topLinks = [
   {
--- src/Compnents/routes/app.routes.jsx
+++ src/Compnents/routes/app.routes.jsx
@@ -1,36 +1,56 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import Home from "../pages/Home";
+
+// About
 import About from "../About/About";
 import WhyPashupati from "../About/WhyPashupati";
 import OurMission from "../About/OurMission";
 import OurVision from "../About/OurVision";
+
+// Services
 import HouseholdShifting from "../services/HouseholdShifting";
 import OfficeRelocation from "../services/OfficeRelocation";
 import VehicleTransportation from "../services/VehicleTransportation";
 import PackingServices from "../services/PackingServices";
 import LoadingUnloading from "../services/LoadingUnloading";
 import StorageServices from "../services/StorageServices";
+
+// Process
 import Packing from "../process/Packing";
 import Loading from "../process/loading";
 import Transportion from "../process/Transportion";
 import Unloading from "../process/Unloading";
 import Delivery from "../process/delivery";
+
+// Blogs / Contact
 import Blogs from "../Blogs/Blogs";
 import ContactUs from "../ContactUs/ContactUs";
 import GetAQuote from "../ContactUs/GetAQuote";
 import Enquiry from "../ContactUs/Enquiry";
+
+// Auth
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+
+// Admin
 import Admin from "../pages/Admin/admin";
+
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      {/* ==================== HOME ==================== */}
+      <Route path="/" element={<Home />} />
+
+      {/* ==================== ABOUT ==================== */}
       <Route path="/about" element={<About />} />
-<Route path="/why-us" element={<WhyPashupati />} />
-<Route path="/mission" element={<OurMission/>} />
-<Route path="/vision" element={<OurVision />} />
+      <Route path="/why-us" element={<WhyPashupati />} />
+      <Route path="/mission" element={<OurMission />} />
+      <Route path="/vision" element={<OurVision />} />
+
+      {/* ==================== SERVICES ==================== */}
       <Route
         path="/services/household-shifting"
         element={<HouseholdShifting />}
@@ -60,33 +80,34 @@ const AppRoutes = () => {
         path="/services/storage"
         element={<StorageServices />}
       />
-      <Route path="/" element={<Navigate to="/packing" replace />} />
 
+      {/* ==================== PROCESS ==================== */}
       <Route path="/packing" element={<Packing />} />
-
       <Route path="/loading" element={<Loading />} />
-
       <Route path="/transportation" element={<Transportion />} />
-
       <Route path="/unloading" element={<Unloading />} />
-
       <Route path="/delivery" element={<Delivery />} />
 
+      {/* ==================== BLOG ==================== */}
+      <Route path="/blog" element={<Blogs />} />
+
+      {/* ==================== CONTACT ==================== */}
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/quote" element={<GetAQuote />} />
+      <Route path="/enquiry" element={<Enquiry />} />
+
+      {/* ==================== AUTH ==================== */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* ==================== ADMIN ==================== */}
+      <Route path="/admin/dashboard" element={<Admin />} />
+
+      {/* ==================== 404 ==================== */}
       <Route
         path="*"
-        element={<Navigate to="/packing" replace />}
+        element={<Navigate to="/" replace />}
       />
-      <Route path="/blog" element={<Blogs/>}/>
-      <Route path="/contact" element={<ContactUs/>}/>
-      <Route path="/quote" element={<GetAQuote/>}/>
-      <Route path="/enquiry" element={<Enquiry/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/admin/dashboard" element={<Admin/>}/>
-
-
-
-
     </Routes>
   );
 };
